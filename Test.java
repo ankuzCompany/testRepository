c946c5086fac40847a86aa17416f62316c919920
Andrew Kuznetsov2 <kamikadze_17@bk.ru>
2012-10-19 22:17:52 +0400

    Add second line

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test{
 	public static void main(String[] args){
-		System.out.println("tessfffsdsdfsdffgsdgsst");
+		System.out.println("another changes");
+		System.out.println("second line");
 	}
 }
